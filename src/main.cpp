3feb03c9edf318e248506affe5be330ca8e9f96a
Suleimen Zhonissov <lmfao159753@mail.ru>
2024-03-12 19:45:28 -0400

    something(?) done

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@
 #include "renderer/renderer_types.h"
 
 class TestSquare {
- private:
+private:
   float n;
 
- public:
+public:
   TestSquare() : n(0) {}
-  void drawSquare(Frame& frame);
+  void drawSquare(Frame &frame);
 };
 
-void TestSquare::drawSquare(Frame& frame) {
+void TestSquare::drawSquare(Frame &frame) {
   n += 0.1;
   float sn = sin(n);
   float cn = cos(n);
@@ -29,14 +29,14 @@ void TestSquare::drawSquare(Frame& frame) {
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   if (argc < 2) {
     std::cout << "Usage: " << argv[0] << " <config file>" << std::endl;
     return 1;
   }
 
   Parser parser;
-  RenderingInfo* info = parser.parseFile(argv[1]);
+  RenderingInfo *info = parser.parseFile(argv[1]);
   if (info == nullptr) {
     std::cout << "Failed to parse file" << std::endl;
     return 1;
@@ -45,20 +45,39 @@ int main(int argc, char** argv) {
   Frame frame;
   Renderer renderer;
 
-  // Instead of drawing the test square,
-  // use the rendering info to render the scene
-  TestSquare ts;
-  ts.drawSquare(frame);
+  Vect origin = {0.0f, 0.0f, -(WIDTH/info->focal_length)};
+
+  for(int y = 0; y < HEIGHT; y++) {
+    for(int x = 0; x < WIDTH; x++) {
+      Vect pixel = {x + 0.5f, y + 0.5f, 0.0f};
+      Vect d = pixel - origin;
+
+      std::vector<Sphere*> spheres = info->spheres;
+      for(int i = 0; i < spheres.size(); i++){
+        float a = d*d;
+        float b = (2 * d) * (origin - spheres[i]->center);
+        float c = (origin - spheres[i]->center) * (origin - spheres[i]->center) - spheres[i]->radius * spheres[i]->radius;
+
+        float discriminant = b*b - 4*a*c;
+        if(discriminant >= 0){
+          float sqrt_disc = sqrt(discriminant);
+          float t1 = (-b + sqrt_disc) / (2*a);
+          float t2 = (-b - sqrt_disc) / (2*a);
+          
+          float t = std::min(t1, t2);
+          frame.setColor(x, y, spheres[i]->material->color);
+        }
+      }
+    }
+  }
 
   if (!renderer.init(frame)) {
     std::cout << "Failed to init renderer" << std::endl;
     return 1;
   }
   while (renderer.render()) {
-    // Animating is extra credit, if you're not doing
-    // animation delete the code inside this loop.
-    frame.clear();
-    ts.drawSquare(frame);
-    renderer.changeFrame(frame);
+
+    // frame.clear();
+    // renderer.changeFrame(frame);
   }
 }
